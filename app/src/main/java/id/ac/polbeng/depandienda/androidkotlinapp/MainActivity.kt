31bfc2ce478459f344c8dc4de6520c71017ffde3
Depandi Enda <depandienda@gmail.com>
2024-10-08 10:25:12 +0700

    Menambahkan fungsi addNumber, resetNumber dan subtractNumber

--- app/src/main/java/id/ac/polbeng/depandienda/androidkotlinapp/MainActivity.kt
+++ app/src/main/java/id/ac/polbeng/depandienda/androidkotlinapp/MainActivity.kt
@@ -1,20 +1,41 @@
 package id.ac.polbeng.depandienda.androidkotlinapp
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var tvCounter: TextView
+    private lateinit var btnAddNumber: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        //layout binding
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        //view binding
+        tvCounter = findViewById(R.id.tvCounter)
+        btnAddNumber = findViewById(R.id.btnAddNumber)
+
+        tvCounter.text = "1"
+    }
+
+    fun addNumber(v: View){
+        val currVal = tvCounter.text.toString().toInt()
+        val nextVal = currVal + 1
+        tvCounter.text = nextVal.toString()
+    }
+
+    fun resetNumber(v: View){
+        tvCounter.text = "0"
+    }
+
+    fun subtractNumber(v: View){
+        val currVal = tvCounter.text.toString().toInt()
+        val nextVal = currVal - 1
+        tvCounter.text = nextVal.toString()
     }
 }
